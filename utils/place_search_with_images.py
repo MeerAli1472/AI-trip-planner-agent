3ae01a4d3628b30a852c6262dbcbb9751eec30bf
Sayed Meer Ali Shah <103332928+MeerAli1472@users.noreply.github.com>
2026-03-12 07:36:48 +0500

    Update place_search_with_images.py

--- utils/place_search_with_images.py
+++ utils/place_search_with_images.py
@@ -2,7 +2,7 @@ import os
 import requests
 import re
 from typing import List, Dict
-from langchain.tools import tool
+from langchain_core.tools import tool
 
 from dotenv import load_dotenv
 from utils.place_info_search import GeoapifyPlaceSearchTool
